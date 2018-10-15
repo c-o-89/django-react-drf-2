cf93067762ddb1b98935fbed17d6f6b0fe45b5d1
Chuk <chukwumaokpalugo@hotmail.com>
2018-10-14 20:30:23 -0400

    Linked react app to django so django server can host react app

--- todo/backend/todo_api/settings.py
+++ todo/backend/todo_api/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'todo_api.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, '..', 'frontend', 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -132,3 +132,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [  os.path.join(BASE_DIR,'..', 'frontend', 'build/static'),]
